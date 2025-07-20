4ce95f04831c31c536091787987aa3aab2e628b1
JeongYeonH <kukuro1995@naver.com>
2025-07-21 01:43:49 +0900

    본격 로직 추가

---
+++ src/main/java/com/yeonny/demo/LegacyTest/LegacyService.java
@@ -0,0 +1,36 @@
+package com.yeonny.demo.LegacyTest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LegacyService {
+    private String[][] paths 
+    = new String[][] {{"0", "health" },{"1", "NA" },{"2", "NA"},{"3", "reverse"},
+                        {"4", "NA"},{"5", "reverse"},{"6", "health"},{"7", "NA"},
+                        {"8", "health"},{"9", "reverse"},{"10", "NA"},{"11", "health"}};
+    private Map<String, int[]> map = new HashMap<>();
+
+    public String move(LegacyDto legacyDto){
+        String name = legacyDto.userName;
+        if(!map.containsKey(name)){
+            map.put(name, new int[]{0, 3, 0, 1});
+        }
+        int[] currUserInfo = map.get(name);
+        
+        int pos = currUserInfo[0];
+        int health = currUserInfo[1];
+        int point = currUserInfo[2];
+        int dir = currUserInfo[3];
+        
+        if(health - legacyDto.moveRange < 0){
+            return "체력이 부족합니다";
+        }
+
+
+        return null;
+    }
+}
