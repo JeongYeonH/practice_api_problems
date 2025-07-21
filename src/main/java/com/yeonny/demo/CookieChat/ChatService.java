df3347ba7cf3586b4545d1f3bef5255c2daa1063
JeongYeonH <kukuro1995@naver.com>
2025-07-22 00:48:24 +0900

    api 하나 완성

--- src/main/java/com/yeonny/demo/CookieChat/ChatService.java
+++ src/main/java/com/yeonny/demo/CookieChat/ChatService.java
@@ -1,5 +1,7 @@
 package com.yeonny.demo.CookieChat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,4 +12,15 @@ import org.springframework.stereotype.Service;
 public class ChatService {
     private Map<String, String[]> userMap = new HashMap<>();
     private Map<String, List<Message>> messageMap = new HashMap<>();
+
+    public String sendMessage(MessageDto messageDto){
+        String roomName = messageDto.roomId;
+        Message message = new Message(roomName , messageDto.text);
+
+        List<Message> messageList = messageMap.getOrDefault(roomName, new ArrayList<>());
+        messageList.add(message);
+        messageMap.put(roomName , messageList);
+
+        return "메시지가 전송되었습니다";
+    }
 }
